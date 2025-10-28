958b04504e80898b5d3bc3fc8e478df017ea3084
Carmine Maggio <carminemaggiom@gmail.com>
2025-10-27 20:50:01 -0400

    refactor: replace MainNavbar.astro with MainNavbar.vue and update imports
    - Deleted MainNavbar.astro and created MainNavbar.vue with a setup script.
    - Updated Resume.astro to import MainNavbar.vue instead of MainNavbar.astro.
    - Added client:idle directive to MainNavbar in Resume.astro.
    - Updated tsconfig.json to include "jsx": "preserve" in compilerOptions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    i18n: {
-        locales: ["es", "en"],
-        defaultLocale: "en",
-        routing: {
-            prefixDefaultLocale: false
-        }
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  i18n: {
+      locales: ["es", "en"],
+      defaultLocale: "en",
+      routing: {
+          prefixDefaultLocale: false
+      }
+  },
+
+  integrations: [vue()]
 })
---
+++ src/components/navigation/MainNavbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  lang: String
+})
+</script>
+
+<template>
+    <nav class="mt-8 xl:mt-12">
+        <!-- Usa lang aquí, por ejemplo: -->
+        <p>Idioma: {{ props.lang }}</p>
+    </nav>
+</template> 
