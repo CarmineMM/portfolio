acdfa169a50ef1dc0e61c1803d21413313809de5
Carmine Maggio <carminemaggiom@gmail.com>
2025-11-03 22:53:47 -0400

    feat: add DockMainNavbar component and integrate it into Resume section; enhance navigation links with icons

---
+++ src/components/navigation/DockMainNavbar.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    lang: {
+        type: String,
+        default: 'es',
+    },
+    links: {
+        type: Array,
+        default: () => [],
+    },
+})
+</script>
+
+<template>
+    <div class="dock lg:hidden">
+        <a
+            :href="link.href"
+            v-for="link in props.links"
+            :key="link.href"
+            class="inline-flex items-center gap-2"
+        >
+            <span class="dock-label">{{ link.label }}</span>
+        </a>
+    </div>
+</template>
